5473f3f548792fe7208931f474dfc200b8840b3f
tJonh619 <tjonh619@gmail.com>
2023-10-21 00:19:03 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/ProductoesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TiendaAsp.Models;
+
+namespace TiendaAsp.Controllers
+{
+    public class ProductoesController : Controller
+    {
+        private TiendaBdContainer db = new TiendaBdContainer();
+
+        // GET: Productoes
+        public ActionResult Index()
+        {
+            var productos = db.Productos.Include(p => p.Proveedor).Include(p => p.Categoria);
+            return View(productos.ToList());
+        }
+
+        // GET: Productoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: Productoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProveedorId = new SelectList(db.Proveedores, "Id", "nombre");
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "Id", "nombre");
+            return View();
+        }
+
+        // POST: Productoes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre,descripcion,precio,existencia,ProveedorId,CategoriaId")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productos.Add(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProveedorId = new SelectList(db.Proveedores, "Id", "nombre", producto.ProveedorId);
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "Id", "nombre", producto.CategoriaId);
+            return View(producto);
+        }
+
+        // GET: Productoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProveedorId = new SelectList(db.Proveedores, "Id", "nombre", producto.ProveedorId);
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "Id", "nombre", producto.CategoriaId);
+            return View(producto);
+        }
+
+        // POST: Productoes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre,descripcion,precio,existencia,ProveedorId,CategoriaId")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProveedorId = new SelectList(db.Proveedores, "Id", "nombre", producto.ProveedorId);
+            ViewBag.CategoriaId = new SelectList(db.Categorias, "Id", "nombre", producto.CategoriaId);
+            return View(producto);
+        }
+
+        // GET: Productoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Productoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            db.Productos.Remove(producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/TiendaBd.edmx.sql
@@ -0,0 +1,116 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 10/20/2023 23:47:38
+-- Generated from EDMX file: C:\Users\tjonh\source\repos\TiendaAsp\Models\TiendaBd.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [TiendaDb];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Productos'
+CREATE TABLE [dbo].[Productos] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [nombre] nvarchar(max)  NOT NULL,
+    [descripcion] nvarchar(max)  NOT NULL,
+    [precio] decimal(18,0)  NOT NULL,
+    [existencia] int  NOT NULL,
+    [ProveedorId] int  NOT NULL,
+    [CategoriaId] int  NOT NULL
+);
+GO
+
+-- Creating table 'Categorias'
+CREATE TABLE [dbo].[Categorias] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [nombre] nvarchar(max)  NOT NULL,
+    [descripcion] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Proveedores'
+CREATE TABLE [dbo].[Proveedores] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [nombre] nvarchar(max)  NOT NULL,
+    [contacto] nvarchar(max)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Productos'
+ALTER TABLE [dbo].[Productos]
+ADD CONSTRAINT [PK_Productos]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Categorias'
+ALTER TABLE [dbo].[Categorias]
+ADD CONSTRAINT [PK_Categorias]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Proveedores'
+ALTER TABLE [dbo].[Proveedores]
+ADD CONSTRAINT [PK_Proveedores]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [ProveedorId] in table 'Productos'
+ALTER TABLE [dbo].[Productos]
+ADD CONSTRAINT [FK_ProveedorProducto]
+    FOREIGN KEY ([ProveedorId])
+    REFERENCES [dbo].[Proveedores]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_ProveedorProducto'
+CREATE INDEX [IX_FK_ProveedorProducto]
+ON [dbo].[Productos]
+    ([ProveedorId]);
+GO
+
+-- Creating foreign key on [CategoriaId] in table 'Productos'
+ALTER TABLE [dbo].[Productos]
+ADD CONSTRAINT [FK_CategoriaProducto]
+    FOREIGN KEY ([CategoriaId])
+    REFERENCES [dbo].[Categorias]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CategoriaProducto'
+CREATE INDEX [IX_FK_CategoriaProducto]
+ON [dbo].[Productos]
+    ([CategoriaId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
